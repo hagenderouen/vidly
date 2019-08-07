fced1aec0df0bd4f8f214532aa82e9d9cd0c7da9
Hagen DeRouen <hagend87@gmail.com>
2019-08-07 13:00:02 -0500

    Add membership type migrations and re-created customer table to add foreign key

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -5,6 +5,10 @@ namespace Vidly.Models
     public class Customer
     {
         public bool IsSubscribedToNewsLetter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
+
+        // SQLite does not support this migration operation ('AddForeignKeyOperation'). For more information, see http://go.microsoft.com/fwlink/?LinkId=723262.
 
         [Required]
         public int Id { get; set; }
