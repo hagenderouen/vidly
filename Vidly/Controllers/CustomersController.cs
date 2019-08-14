9152aafc7b8304483f46eeeae075083f1e796dd3
Hagen DeRouen <hagend87@gmail.com>
2019-08-14 11:38:56 -0500

    Add Anti-forgery tokens to customer model and view.

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -37,6 +37,7 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Save(Customer customer)
         {
             if (!ModelState.IsValid)
